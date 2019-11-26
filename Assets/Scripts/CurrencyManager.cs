1f7ee93e9186827938e9648867e13f91e80a02c0
Torresus5 <mazylis.tomas@gmail.com>
2019-11-26 21:58:05 +0100

    SolarNation Gaame

---
+++ Assets/Scripts/CurrencyManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyManager : MonoBehaviour
+{
+    // this script calculates the amount of currency the player earns each game tick
+    static public float CurrencyIncrease;
+    public Text increaseText;
+    [Header("Public for testing purposes")]
+
+
+    private float SolarPanels;
+    private float WindTurbines;
+    private float HydroStations;
+    private float CoalPlants;
+    private float GasPlants;
+    private float NuclearPlants;
+
+    [Header("How much each generator generates")]
+    public static float ValueOfSolarPanels = 12;
+    public static float ValueOfWindTurbines = 40;
+    public static float ValueOfHydroStations = 90000;
+    public static float ValueOfCoalPlants = 250;
+    public static float ValueOfGasPlants = 250;
+    public static float ValueOfNuclearPlants = 3500;
+
+    [Header("How much each generator costs")]
+    public static float CostOfSolarPanels = 100;
+    public static float CostOfWindTurbines = 4000;
+    public static float CostOfHydroStations = 10000000;
+    public static float CostOfCoalPlants = 20000;
+    public static float CostOfGasPlants = 300000;
+    public static float CostOfNuclearPlants = 5000000;
+
+    public static Text solarTemp;
+    public Text SolarInspector;
+
+    public static Text windTemp;
+    public Text WindInspector;
+
+    public static Text coalTemp;
+    public Text CoalInspector;
+
+    public static Text hydroTemp;
+    public Text HydroInspector;
+
+    public static Text nuclearTemp;
+    public Text NuclearInspector;
+
+    public static Text gasTemp;
+    public Text GasInspector;
+
+    private void Awake()
+    {
+        solarTemp = SolarInspector;
+        gasTemp = GasInspector;
+        nuclearTemp = NuclearInspector;
+        coalTemp = CoalInspector;
+        hydroTemp = HydroInspector;
+        windTemp = WindInspector;
+
+    }
+    void Update()
+    {
+        solarTemp.text = MoneyConverter.Instance.CurrencyToString(CostOfSolarPanels) + "$";
+        windTemp.text = MoneyConverter.Instance.CurrencyToString(CostOfWindTurbines) + "$";
+        coalTemp.text = MoneyConverter.Instance.CurrencyToString(CostOfCoalPlants) + "$";
+        gasTemp.text = MoneyConverter.Instance.CurrencyToString(CostOfGasPlants) + "$";
+        nuclearTemp.text = MoneyConverter.Instance.CurrencyToString(CostOfNuclearPlants) + "$";
+        hydroTemp.text = MoneyConverter.Instance.CurrencyToString(CostOfHydroStations) + "$";
+
+        increaseText.text = MoneyConverter.Instance.CurrencyToString(CurrencyIncrease) + "/s";
+        if (DayNight.day == false)
+        {
+            CurrencyIncrease = WindTurbines + HydroStations + CoalPlants + GasPlants + NuclearPlants;
+        }
+        if (DayNight.day == true)
+        {
+            CurrencyIncrease = SolarPanels + WindTurbines + HydroStations + CoalPlants + GasPlants + NuclearPlants;
+        }
+        
+        SolarPanels = StatsManager.AmountOfSolarPanels * ValueOfSolarPanels * Upgrades.SolarMultiplier;
+        WindTurbines = StatsManager.AmountOfWindTurbines * ValueOfWindTurbines * Upgrades.WindMultiplier;
+        HydroStations = StatsManager.AmountOfHydroStations * ValueOfHydroStations * Upgrades.HydroMultiplier;
+        CoalPlants = StatsManager.AmountOfCoalPlants * ValueOfCoalPlants * Upgrades.CoalMultiplier;
+        GasPlants = StatsManager.AmountOfGasPlants * ValueOfGasPlants * Upgrades.GasMultiplier;
+        NuclearPlants = StatsManager.AmountOfNuclearPlants * ValueOfNuclearPlants * Upgrades.NuclearMultiplier;
+    }
+}
---
+++ Assets/Scripts/Upgrades.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Upgrades : MonoBehaviour
+{
+    // This script takes care of upgrades and the multipliers coming with it
+    static public float totalUpgrades = 0;
+
+    static public float SolarMultiplier = 1;
+    static public float WindMultiplier = 1;
+    static public float HydroMultiplier = 1;
+    static public float CoalMultiplier = 1;
+    static public float GasMultiplier = 1;
+    static public float NuclearMultiplier = 1;
+
+    static public float SolarUpgradeCost = 1000;
+    static public float WindUpgradeCost = 10000;
+    static public float HydroUpgradeCost = 100000000;
+    static public float CoalUpgradeCost = 50000;
+    static public float GasUpgradeCost = 900000;
+    static public float NuclearUpgradeCost = 10000000;
+
+    public static Text solarTemp;
+    public Text SolarInspector;
+
+    public static Text windTemp;
+    public Text WindInspector;
+
+    public static Text coalTemp;
+    public Text CoalInspector;
+
+    public static Text hydroTemp;
+    public Text HydroInspector;
+
+    public static Text nuclearTemp;
+    public Text NuclearInspector;
+
+    public static Text gasTemp;
+    public Text GasInspector;
+
+    private void Awake()
+    {
+        solarTemp = SolarInspector;
+        gasTemp = GasInspector;
+        nuclearTemp = NuclearInspector;
+        coalTemp = CoalInspector;
+        hydroTemp = HydroInspector;
+        windTemp = WindInspector;
+        
+    }
+    private void Update()
+    {
+        solarTemp.text = MoneyConverter.Instance.CurrencyToString(SolarUpgradeCost) + "$";
+        windTemp.text = MoneyConverter.Instance.CurrencyToString(WindUpgradeCost) + "$";
+        coalTemp.text = MoneyConverter.Instance.CurrencyToString(CoalUpgradeCost) + "$";
+        gasTemp.text = MoneyConverter.Instance.CurrencyToString(GasUpgradeCost) + "$";
+        nuclearTemp.text = MoneyConverter.Instance.CurrencyToString(NuclearUpgradeCost) + "$";
+        hydroTemp.text = MoneyConverter.Instance.CurrencyToString(HydroUpgradeCost) + "$";
+    }
+
+    public void SolarMultiplierFunction()
+    {
+        if (StatsManager.Currency > SolarUpgradeCost)
+        {
+            SolarMultiplier++;
+            StatsManager.Currency -= SolarUpgradeCost;
+            float temp = Mathf.RoundToInt(SolarUpgradeCost * 2f);
+            solarTemp.text = temp.ToString() + "$";
+            SolarUpgradeCost = temp;
+            totalUpgrades++;
+        }
+    }
+    public void WindMultiplierFunction()
+    {
+        if (StatsManager.Currency > WindUpgradeCost)
+        {
+            WindMultiplier++;
+            StatsManager.Currency -= WindUpgradeCost;
+            float temp = Mathf.RoundToInt(WindUpgradeCost * 2f);
+            windTemp.text = MoneyConverter.Instance.CurrencyToString(temp) + "$";
+            WindUpgradeCost = temp;
+            totalUpgrades++;
+        }
+    }
+    public void HydroMultiplierFunction()
+    {
+        if (StatsManager.Currency > HydroUpgradeCost)
+        {
+            HydroMultiplier++;
+            StatsManager.Currency -= HydroUpgradeCost;
+            float temp = Mathf.RoundToInt(HydroUpgradeCost * 2f);
+            hydroTemp.text = MoneyConverter.Instance.CurrencyToString(temp) + "$";
+            HydroUpgradeCost = temp;
+            totalUpgrades++;
+        }
+    }
+    public void CoalMultiplierFunction()
+    {
+        if (StatsManager.Currency > CoalUpgradeCost)
+        {
+            CoalMultiplier++;
+            StatsManager.Currency -= CoalUpgradeCost;
+            float temp = Mathf.RoundToInt(CoalUpgradeCost * 2f);
+            coalTemp.text = MoneyConverter.Instance.CurrencyToString(temp) + "$";
+            CoalUpgradeCost = temp;
+            totalUpgrades++;
+        }
+    }
+    public void GasMultiplierFunction()
+    {
+        if (StatsManager.Currency > GasUpgradeCost)
+        {
+            GasMultiplier ++;
+            StatsManager.Currency -= GasUpgradeCost;
+            float temp = Mathf.RoundToInt(GasUpgradeCost * 2f);
+            gasTemp.text = MoneyConverter.Instance.CurrencyToString(temp) + "$";
+            GasUpgradeCost = temp;
+            totalUpgrades++;
+
+        }
+    }
+    public void NuclearMultiplierFunction()
+    {
+        if (StatsManager.Currency > NuclearUpgradeCost)
+        {
+            NuclearMultiplier++;
+            StatsManager.Currency -= NuclearUpgradeCost;
+            float temp = Mathf.RoundToInt(NuclearUpgradeCost * 2f);
+            nuclearTemp.text = MoneyConverter.Instance.CurrencyToString(temp) + "$";
+            NuclearUpgradeCost = temp;
+            totalUpgrades++;
+        }
+    }
+}
